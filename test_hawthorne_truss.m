ef0c572bff73c753b77e745178d0fc444379f2d8
Staleychen <cst19555077888@outlook.com>
2025-07-06 14:05:48 +0100

    Add truss stiffness functions and demo

--- test_hawthorne_truss.m
+++ test_hawthorne_truss.m
@@ -8,3 +8,7 @@ for k = 1:5
     e = elems(k);
     fprintf('%d %d %s A=%.2f\n', e.i, e.j, e.type, e.A);
 end
+
+K = assembleK(nodes, elems, E);
+fprintf('size(K) = %dx%d\n', size(K,1), size(K,2));
+fprintf('rank(full(K)) = %d\n', rank(full(K)));
